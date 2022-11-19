a1fa49d301abd392d9c4da89ea4eefc096b045cd
Michael <m-silkov@inbox.ru>
2022-11-19 23:05:43 +0300

    fix:refactor popup components

--- mesto/src/components/App.js
+++ mesto/src/components/App.js
@@ -12,12 +12,11 @@ function App() {
 			<Main />
 			<Footer />
 
-			<PopupWithForm name="edit-profile" title="Редактировать профиль" />
-
-			<article className="popup popup_type_add-card">
-				<div className="popup__container">
-					<h2 className="popup__title">Новое место</h2>
-					<form name="create-card" className="popup__form">
+			<PopupWithForm
+				name="edit-profile"
+				title="Редактировать профиль"
+				children={
+					<React.Fragment>
 						<fieldset className="popup__input-group">
 							<input
 								id="place-input"
@@ -51,30 +50,65 @@ function App() {
 								className="img-input-error popup__input-error"
 							/>
 						</fieldset>
-						<button type="submit" className="popup__submit-btn">
-							Создать
-						</button>
-					</form>
-					<button type="reset" className="popup__close-btn" />
-				</div>
-			</article>
+					</React.Fragment>
+				}
+				button="Сохранить"
+			/>
 
-			<article className="popup popup_type_confirmation">
-				<div className="popup__container popup__container_content_confirmation">
-					<h2 className="popup__title">Вы уверены?</h2>
-					<form name="confirm" className="popup__form">
-						<button type="submit" className="popup__submit-btn">
-							Да
-						</button>
-					</form>
-					<button type="reset" className="popup__close-btn" />
-				</div>
-			</article>
+			<PopupWithForm
+				name="add-card"
+				title="Новое место"
+				children={
+					<React.Fragment>
+						<fieldset className="popup__input-group">
+							<input
+								id="place-input"
+								type="text"
+								name="name"
+								className="popup__input popup__input_type_img-title"
+								placeholder="Название"
+								defaultValue=""
+								minLength={2}
+								maxLength={30}
+								required=""
+							/>
+							<span
+								id="place-input-error"
+								className="place-input-error popup__input-error"
+							/>
+						</fieldset>
+						<fieldset className="popup__input-group">
+							<input
+								id="img-input"
+								type="text"
+								name="link"
+								className="popup__input popup__input_type_img-link"
+								placeholder="Ссылка на картинку"
+								defaultValue=""
+								pattern="https?://.+"
+								required=""
+							/>
+							<span
+								id="img-input-error"
+								className="img-input-error popup__input-error"
+							/>
+						</fieldset>
+					</React.Fragment>
+				}
+				button="Создать"
+			/>
+			<PopupWithForm
+				name="confirmation"
+				title="Вы уверены?"
+				button="Да"
+				popupContent="popup__container_content_confirmation"
+			/>
 
-			<article className="popup popup_type_edit-avatar">
-				<div className="popup__container popup__container_content_edit-avatar">
-					<h2 className="popup__title">Обновить аватар</h2>
-					<form name="confirm" className="popup__form">
+			<PopupWithForm
+				name="edit-avatar"
+				title="Обновить аватар"
+				children={
+					<React.Fragment>
 						<fieldset className="popup__input-group">
 							<input
 								id="owner-avatar"
@@ -91,13 +125,11 @@ function App() {
 								className="img-input-error popup__input-error"
 							/>
 						</fieldset>
-						<button type="submit" className="popup__submit-btn">
-							Сохранить
-						</button>
-					</form>
-					<button type="reset" className="popup__close-btn" />
-				</div>
-			</article>
+					</React.Fragment>
+				}
+				button="Сохранить"
+				popupContent="popup__container_content_edit-avatar"
+			/>
 
 			<article className="popup popup_type_zoom-img">
 				<div className="popup__container popup__container_content_image">
--- mesto/src/components/PopupWithForm.js
+++ mesto/src/components/PopupWithForm.js
@@ -1,45 +1,12 @@
 export default function PopupWithForm(props) {
 	return (
 		<article className={`popup popup_type_${props.name}`}>
-			<div className="popup__container">
+			<div className={`popup__container ${props.popupContent}`}>
 				<h2 className="popup__title">{props.title}</h2>
-				<form name="edit-form" className="popup__form">
-					<fieldset className="popup__input-group">
-						<input
-							id="name-input"
-							type="text"
-							name="name"
-							className="popup__input popup__input_type_name"
-							placeholder="Ваше Имя"
-							defaultValue=""
-							minLength={2}
-							maxLength={40}
-							required=""
-						/>
-						<span
-							id="name-input-error"
-							className="name-input-error popup__input-error"
-						/>
-					</fieldset>
-					<fieldset className="popup__input-group">
-						<input
-							id="prof-input"
-							type="text"
-							name="about"
-							className="popup__input popup__input_type_profession"
-							placeholder="Ваша профессия"
-							defaultValue=""
-							minLength={2}
-							maxLength={200}
-							required=""
-						/>
-						<span
-							id="prof-input-error"
-							className="prof-input-error popup__input-error"
-						/>
-					</fieldset>
+				<form name={props.name} className="popup__form">
+					{props.children}
 					<button type="submit" className="popup__submit-btn">
-						Сохранить
+						{props.button}
 					</button>
 				</form>
 				<button type="reset" className="popup__close-btn" />
