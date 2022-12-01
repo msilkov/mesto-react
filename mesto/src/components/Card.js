f7987ea843fc061979d41f5f54d392deaf8683cb
Michael <m-silkov@inbox.ru>
2022-12-01 15:27:00 +0300

    feat: make owners delete btn and likes vissible

--- mesto/src/components/Card.js
+++ mesto/src/components/Card.js
@@ -5,12 +5,17 @@ export default function Card(props) {
 	const currentUser = useContext(userContext);
 
 	const isOwn = props.card.ownerId === currentUser._id;
-	console.log(currentUser._id)
-	console.log(props.card.ownerId)
-	console.log(isOwn)
-	const cardDeleteButtonClassName = (
-		`card__remove-btn ${isOwn ? 'card__remove-btn_type_visible' : 'card__remove-btn_type_hidden'}`
-	); 
+
+	const cardDeleteButtonClassName = `card__remove-btn ${
+		isOwn ? "card__remove-btn_type_visible" : "card__remove-btn_type_hidden"
+	}`;
+
+	const isOwnLiked = props.card.likes.some((i) => i._id === currentUser._id);
+	console.log(isOwnLiked);
+	// Создаём переменную, которую после зададим в `className` для кнопки лайка
+	const cardLikeButtonClassName = `${
+		isOwnLiked ? "card__like-btn card__like-btn_active" : "card__like-btn"
+	}`;
 
 	function handleClick() {
 		props.onClick(props.card);
@@ -27,7 +32,7 @@ export default function Card(props) {
 			<div className="card__description">
 				<h2 className="card__title">{props.card.name}</h2>
 				<div className="card-likes">
-					<button type="button" className="card__like-btn" />
+					<button type="button" className={cardLikeButtonClassName} />
 					<p
 						className={`card__like-counter ${
 							props.card.likes.length ? " card__like-counter_active" : ""
